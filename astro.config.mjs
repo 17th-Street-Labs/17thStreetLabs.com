13ebb42d9fb1d8ebde600f1f4682c9445a00a9d1
Dan Levy <justsml@gmail.com>
2026-09-13 20:07:05 -0600

    feat: add HeroThreads component and remove Prism
    - Introduced a new HeroThreads component that utilizes WebThreads for a dynamic visual effect.
    - Removed the Prism component to streamline the visual elements on the homepage.
    - Added a new WebThreads component for rendering threads with customizable properties.
    - Created a reusable Button component with variants and styles.
    - Updated global styles to include Tailwind CSS and custom themes.
    - Enhanced TypeScript configuration for improved JSX support and path aliases.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+
+import react from "@astrojs/react";
 
 export default defineConfig({
   site: "https://17thstreetlabs.com",
   output: "static",
   trailingSlash: "always",
-  integrations: [sitemap()],
+  integrations: [sitemap(), react()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
